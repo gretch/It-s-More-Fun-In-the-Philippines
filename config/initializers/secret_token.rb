d8cbdf0c48f23baf802e8bc5c4cd0cf42d327eb2
Gretch Gapol <gapolgretchen@gmail.com>
2012-02-01 17:23:30 +0800

    Because it's more fun in the Philippines [initial]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imfitf2::Application.config.secret_token = 'c49d4db124cff94d7a8467deb57c65c3c1f824f03bdbc420d6fc712dc698892459865d1b2e3971a5e380fa895a4dec4b3b382d22ef89da00903257a2a892b754'
